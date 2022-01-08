47403f8413be5bc73b2c780ed2c11babaeb441d3
WaelNalouti <wael99g1@gmail.com>
2022-01-08 10:01:01 +0100

    :sparkles: added utlis func

--- src/functions/counter.js
+++ src/functions/counter.js
@@ -1,12 +1,16 @@
-function timeCounter(stausBarItem) {
-  let hours = 0;
-  let minutes = 0;
-  let seconds = 0;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
 
-  let displayHours = "00";
-  let displayMinutes = "00";
-  let displaySeconds = "00";
-  let timeSpent = "00:00:00";
+let displayHours = "00";
+let displayMinutes = "00";
+let displaySeconds = "00";
+let timeSpent = "00:00:00";
+
+let interval;
+
+//Start the timer from 00:00:00
+function StartTimeCounter(stausBarItem) {
   function IncrementTime() {
     seconds++;
     if (seconds > 59) {
@@ -40,9 +44,79 @@ function timeCounter(stausBarItem) {
     stausBarItem.text = `$(watch) ${timeSpent}`;
   }
 
-  setInterval(IncrementTime, 1000);
+  if (!interval) {
+    interval = setInterval(IncrementTime, 1000);
+  }
+  stausBarItem.tooltip = "Time elapsed since work session started";
+}
+
+//Pause the counter at the last recorded time
+function PauseTimeCounter(stausBarItem) {
+  clearInterval(interval);
+  interval = null;
+  timeSpent = `${displayHours}:${displayMinutes}:${displaySeconds}`;
+  stausBarItem.text = `$(watch) Timer paused [${timeSpent}]`;
+  stausBarItem.tooltip = "Timer paused";
+}
+
+//Resume the counter from the last recorded time
+function ResumeTimeCounter(stausBarItem) {
+  function IncrementTime() {
+    seconds++;
+    if (seconds > 59) {
+      seconds = 0;
+      minutes++;
+
+      if (minutes > 59) {
+        minutes = 0;
+        hours++;
+      }
+    }
+
+    if (seconds < 10) {
+      displaySeconds = `0${seconds}`;
+    } else {
+      displaySeconds = seconds;
+    }
+
+    if (minutes < 10) {
+      displayMinutes = `0${minutes}`;
+    } else {
+      displayMinutes = minutes;
+    }
+    if (hours < 10) {
+      displayHours = `0${hours}`;
+    } else {
+      displayHours = hours;
+    }
+
+    timeSpent = `${displayHours}:${displayMinutes}:${displaySeconds}`;
+    stausBarItem.text = `$(watch) ${timeSpent}`;
+  }
+  if (!interval) {
+    interval = setInterval(IncrementTime, 1000);
+  }
+  stausBarItem.tooltip = "Time elapsed since work session started";
+}
+
+//Reset the counter from the last recorded time
+function ResetTimeCounter(stausBarItem) {
+  clearInterval(interval);
+  interval = null;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  displayHours = "00";
+  displayMinutes = "00";
+  displaySeconds = "00";
+  timeSpent = "00:00:00";
+  stausBarItem.text = `$(watch) ${timeSpent}`;
+  stausBarItem.tooltip = "Work session time tracker is not activated";
 }
 
 module.exports = {
-  timeCounter,
+  StartTimeCounter,
+  PauseTimeCounter,
+  ResumeTimeCounter,
+  ResetTimeCounter,
 };
